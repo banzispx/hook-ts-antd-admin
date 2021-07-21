93476db085500422dbf99da660ddc1e27cdc9ffe
孙培显 <sunpeixian@hikvision.com.cn>
2021-07-21 20:20:12 +0800

    测试

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"><img src={logo} className="App-logo" alt="logo" /><p>Edit <code>src/App.tsx</code> and save to reload.
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -14,7 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a></header>
+        </a>
+      </header>
     </div>
   );
 }
